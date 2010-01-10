dd9ca542e2abc30e27ade6a9a8357bce645b186a
Philip Tellis <philip@bluesmoon.info>
2010-01-10 09:30:21 -0800

    add gif for latency test

--- mkzip.sh
+++ mkzip.sh
@@ -1,2 +1,2 @@
 VERSION=`awk '/^	version: ".*",/ { print $2; }' bw-test.js | sed -e 's/[^a-zA-Z0-9\.-]//g'`
-zip -r bw-test-$VERSION.zip bw-test.js image*.png README tests/*.html
+zip -r bw-test-$VERSION.zip bw-test.js image*.png image-l.gif README tests/*.html
